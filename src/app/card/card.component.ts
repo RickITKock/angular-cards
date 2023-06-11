317952255d6e761891eb13270d701f66e9a913cf
Rick Kock <rit.kock@gmail.com>
2023-06-11 10:35:41 +0200

    Implement card component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
+  @Input() title = '';
+  @Input() imageUrl = '';
+  @Input() content = '';
+  @Input() username = '';
 
+  constructor() {}
+
+  ngOnInit() {}
 }
